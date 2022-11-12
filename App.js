fe28100cf0c7a45909146568bee7af5c10cd8603
ivan <ivannovaa.24@gmail.com>
2022-11-12 15:39:19 +0700

    scrollview & list

--- App.js
+++ App.js
@@ -1,24 +1,49 @@
-import { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("ivan");
-  function handleClick() {
-    setName("Ardiansyah");
-  }
+  const listPeople = [
+    {
+      name: "ivan",
+      key: 1,
+    },
+    {
+      name: "nova",
+      key: 2,
+    },
+    {
+      name: "ardiansyah",
+      key: 3,
+    },
+    {
+      name: "irvan",
+      key: 4,
+    },
+    {
+      name: "evan",
+      key: 5,
+    },
+    {
+      name: "ervan",
+      key: 6,
+    },
+    {
+      name: "ervano",
+      key: 7,
+    },
+  ];
+
   return (
     <View style={styles.container}>
-      <View style={styles.bgPink}>
-        <Text style={styles.textBold}>Hello Word</Text>
+      <View style={styles.containerNavbar}>
+        <Text style={styles.textBold}>List of People:</Text>
       </View>
-      <View style={styles.secondContainer}>
-        <Text style={styles.textCenter}>My name is {name}</Text>
-        <View style={styles.buttonStyle}>
-          <Button title="Update My Name!" onPress={handleClick} />
-        </View>
-      </View>
-      <StatusBar style="auto" />
+      <ScrollView style={styles.scrollContainer}>
+        {listPeople.map((item) => (
+          <View key={item.key} style={styles.listContainer}>
+            <Text style={styles.listText}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -27,23 +52,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  scrollContainer: {
+    paddingHorizontal: 20,
+  },
+  containerNavbar: {
+    padding: 20,
   },
   textBold: {
     fontWeight: "bold",
+    fontSize: 20,
   },
-  bgPink: {
-    backgroundColor: "pink",
-    padding: 20,
-  },
-  secondContainer: {
+  listContainer: {
+    backgroundColor: "blue",
+    borderRadius: 10,
+    padding: 30,
     marginTop: 20,
   },
-  textCenter: {
-    textAlign: "center",
-  },
-  buttonStyle: {
-    marginTop: 10,
+  listText: {
+    fontWeight: "bold",
+    color: "white",
   },
 });
