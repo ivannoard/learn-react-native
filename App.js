80e056aee1986c76043f426a26afa4ac66fd2731
ivan <ivannovaa.24@gmail.com>
2022-11-12 15:16:24 +0700

    text input & state

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("ivan");
-  function handleClick() {
-    setName("Ardiansyah");
-  }
+  const [age, setAge] = useState("21");
+
   return (
     <View style={styles.container}>
-      <View style={styles.bgPink}>
-        <Text style={styles.textBold}>Hello Word</Text>
-      </View>
-      <View style={styles.secondContainer}>
-        <Text style={styles.textCenter}>My name is {name}</Text>
-        <View style={styles.buttonStyle}>
-          <Button title="Update My Name!" onPress={handleClick} />
-        </View>
-      </View>
+      <Text style={styles.heading}>My name is {name}</Text>
+      <Text style={styles.heading}>My age is {age}</Text>
+      <Text>Enter Name:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="e.g John Doe"
+        onChangeText={(val) => setName(val)}
+      />
+      <Text>Enter Age:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="e.g John Doe"
+        onChangeText={(val) => setAge(val)}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -30,20 +35,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  textBold: {
+  heading: {
     fontWeight: "bold",
+    fontSize: 20,
+    marginBottom: 10,
   },
-  bgPink: {
-    backgroundColor: "pink",
-    padding: 20,
-  },
-  secondContainer: {
-    marginTop: 20,
-  },
-  textCenter: {
-    textAlign: "center",
-  },
-  buttonStyle: {
-    marginTop: 10,
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    borderRadius: 20,
+    margin: 10,
+    padding: 10,
+    width: 200,
   },
 });
