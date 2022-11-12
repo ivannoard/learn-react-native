8be5989c1c69cefca59cd0120dc2ce2269057f2d
ivan <ivannovaa.24@gmail.com>
2022-11-12 16:17:06 +0700

    touchable component + delete logic with filter

--- App.js
+++ App.js
@@ -1,24 +1,73 @@
 import { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("ivan");
-  function handleClick() {
-    setName("Ardiansyah");
+  const [listPeople, setListPeople] = useState([
+    {
+      name: "ivan",
+      id: 1,
+    },
+    {
+      name: "nova",
+      id: 2,
+    },
+    {
+      name: "ardiansyah",
+      id: 3,
+    },
+    {
+      name: "irvan",
+      id: 4,
+    },
+    {
+      name: "evan",
+      id: 5,
+    },
+    {
+      name: "ervan",
+      id: 6,
+    },
+    {
+      name: "ervano",
+      id: 7,
+    },
+  ]);
+
+  function handlePress(id) {
+    setListPeople((prevState) => {
+      return prevState.filter((item) => item.id !== id);
+    });
   }
+
   return (
     <View style={styles.container}>
-      <View style={styles.bgPink}>
-        <Text style={styles.textBold}>Hello Word</Text>
+      <View style={styles.containerNavbar}>
+        <Text style={styles.textBold}>List of People:</Text>
       </View>
-      <View style={styles.secondContainer}>
-        <Text style={styles.textCenter}>My name is {name}</Text>
-        <View style={styles.buttonStyle}>
-          <Button title="Update My Name!" onPress={handleClick} />
-        </View>
-      </View>
-      <StatusBar style="auto" />
+      <FlatList
+        style={styles.scrollContainer}
+        keyExtractor={(item) => item.id}
+        data={listPeople}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => handlePress(item.id)}>
+            <Text style={styles.listText}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+      {/* <ScrollView style={styles.scrollContainer}>
+        {listPeople.map((item) => (
+          <View key={item.key} style={styles.listContainer}>
+            <Text style={styles.listText}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
     </View>
   );
 }
@@ -27,23 +76,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  scrollContainer: {
+    paddingHorizontal: 20,
+  },
+  containerNavbar: {
+    padding: 20,
+    paddingTop: 40,
   },
   textBold: {
     fontWeight: "bold",
+    fontSize: 20,
   },
-  bgPink: {
-    backgroundColor: "pink",
-    padding: 20,
-  },
-  secondContainer: {
+  listText: {
+    fontWeight: "bold",
+    color: "white",
+    backgroundColor: "blue",
+    borderRadius: 10,
+    padding: 30,
     marginTop: 20,
-  },
-  textCenter: {
-    textAlign: "center",
-  },
-  buttonStyle: {
-    marginTop: 10,
   },
 });
