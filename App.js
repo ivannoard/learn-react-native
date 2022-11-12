6dd13b70a32a3f81fb0057cb78a257d82c7c8a2e
ivan <ivannovaa.24@gmail.com>
2022-11-12 14:58:56 +0700

    button, text, view, state

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("ivan");
+  function handleClick() {
+    setName("Ardiansyah");
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.bgPink}>
+        <Text style={styles.textBold}>Hello Word</Text>
+      </View>
+      <View style={styles.secondContainer}>
+        <Text style={styles.textCenter}>My name is {name}</Text>
+        <View style={styles.buttonStyle}>
+          <Button title="Update My Name!" onPress={handleClick} />
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textBold: {
+    fontWeight: "bold",
+  },
+  bgPink: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  secondContainer: {
+    marginTop: 20,
+  },
+  textCenter: {
+    textAlign: "center",
+  },
+  buttonStyle: {
+    marginTop: 10,
   },
 });
